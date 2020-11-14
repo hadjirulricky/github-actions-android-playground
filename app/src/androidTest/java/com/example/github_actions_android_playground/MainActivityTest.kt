41b3be8a188a61c9d50880b94c9f4a04c05fbafd
Ricky Hadjirul <hadjirulricky@gmail.com>
2020-11-14 13:49:46 +0800

    Force fail

--- app/src/androidTest/java/com/example/github_actions_android_playground/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/github_actions_android_playground/MainActivityTest.kt
@@ -4,6 +4,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isNotChecked
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.junit.Before
 import org.junit.Test
@@ -17,6 +18,6 @@ class MainActivityTest {
 
     @Test
     fun test() {
-        onView(withId(R.id.textview_hello_world)).check(matches(isDisplayed()))
+        onView(withId(R.id.textview_hello_world)).check(matches(isNotChecked()))
     }
 }
--- app/src/main/java/com/example/github_actions_android_playground/MainActivity.kt
+++ app/src/main/java/com/example/github_actions_android_playground/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.github_actions_android_playground
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
